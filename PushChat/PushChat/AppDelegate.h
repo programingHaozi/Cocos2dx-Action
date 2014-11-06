d806c3e1d47cd131dde0f3ecc7a004a0d20134b8
chenhao <229520281@qq.com>
2014-11-06 17:18:22 +0800

    RemoteNotifiCation
    RemoteNotifiCation

---
+++ PushChat/PushChat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PushChat
+//
+//  Created by chenhao on 14/11/6.
+//  Copyright (c) 2014年 avcon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PushChat/PushChat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PushChat
+//
+//  Created by chenhao on 14/11/6.
+//  Copyright (c) 2014年 avcon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
